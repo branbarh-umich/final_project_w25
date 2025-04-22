02553d42eaed98fd1e410d5c43b284fceb5afe52
Brandon Barham <branbarh@umich.edu>
2025-04-22 08:45:14 -0400

    Add content to "About" page

--- src/app/(meta)/meta.ts
+++ src/app/(meta)/meta.ts
@@ -126,6 +126,108 @@ export function getContent(page: string): Content[] {
       }
     ];
   }
+  if (page === "about") {
+    return [
+      {
+        type: "header",
+        data: { value: "All About Brandon", fragmentID: "all-about-brandon" }
+      },
+      {
+        type: "text",
+        data: { value: "Hey there!" }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "I'm Brandon Barham, a student at the University of Michigan in Ann Arbor. I'm majoring in Computer Science through the College of Engineering, where I've been building a strong foundation in software development, critical thinking, and design. After this semester, in May of 2025, I’ll officially be graduating with my bachelor’s degree."
+        }
+      },
+      {
+        type: "photoGroup",
+        data: { photos: [{ src: `${config.basePath}/assets/images/next-to-tree.jpg`, alt: "Brandon wearing light sports/hiking clothes, leaning on a large fallen tree." }] }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "As much as I love spending time in nature, my desk is my natural habitat. I’ve always been drawn towards computers and creative problem solving, whether it be for a proof in my cryptography class or a bug in my React app. I have a knack for automating every little task in life, even if it only saves me three seconds in the end."
+        }
+      },
+      {
+        type: "text",
+        data: {
+          value: "You might be wondering how I got into programming to begin with—I think this excerpt from my University of Michigan admissions essay speaks for itself:"
+        }
+      },
+      {
+        type: "text",
+        data: {
+          value: "“Was it destiny? Fate? Chance? Honestly, not even I know."
+        }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "Let me provide a bit of context. Picture a small party, set in a small colonial, filled with the pleasant aura of a Thanksgiving dinner. An older man, probably in his late 50s, walks over to me. He seems familiar, but I can’t quite put my finger on why—he greets me. Distracted by the potatoes, I don't absorb much of what he says, but one key phrase stands out: Khan Academy. That sole phrase might not carry much significance to an outsider, but I attribute the course of the next decade of my life to that singular moment."
+        }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "With my age still in the single-digits, I wrote my first program on Khan Academy. Now, I use the term “wrote” loosely here; in reality, I just copied someone else’s program and fiddled with it a little. However, from then on, my curiosity and urge to learn shaped my present-day knowledge. I started writing more complex programs, employing real-world algorithms, and eventually branched out to building web pages and working with Node.js.”"
+        }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "Since writing this essay, I’ve further honed my skills through coursework, internships, and personal projects—but I know I’m just getting started. I’m very excited to see what the future has to offer."
+        }
+      },
+      {
+        type: "spacer",
+        data: {}
+      },
+      {
+        type: "header",
+        data: { value: "My Mindset", fragmentID: "my-mindset" }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "My core philosophy is that a minute today is worth two tomorrow. This guiding principle drives me to always put in maximal effort, even for the small things. I give my everything today, knowing I will reap the benefits down the line."
+        }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "There’s no better example of this mindset paying off than the car I recently bought—my Toyota GR86. I’ve been saving up to buy a car once I graduated college for years, and through devout research, disciplined financial management, and driven focus (no pun intended), I achieved that dream. I was never chasing a car, though—I was chasing the freedom it offered, and the effort it reflected. This car represents my two minutes paying their dividends."
+        }
+      },
+      {
+        type: "photoGroup",
+        data: { photos: [{ src: `${config.basePath}/assets/images/gr86.jpg`, alt: "A 2025 Trueno Blue GR86 after a fresh rain, sitting in the parking lot of a Toyota dealership." }] }
+      },
+      {
+        type: "text",
+        data: {
+          value:
+            "Now, every time I get behind the wheel, I'm reminded that the time I put into today shapes the life I get to live tomorrow. It’s a reminder that hard work and determination aren’t just about immediate results—they’re about building something that lasts."
+        }
+      },
+      {
+        type: "text",
+        data: {
+          value: "Also, yes, the GR86 is a manual."
+        }
+      }
+    ];
+  }
   return [
     {
       type: "header",
@@ -199,6 +301,32 @@ export function getContent(page: string): Content[] {
 
 export function getSectionGroups(page: string): SectionGroup[] {
   // [TODO] Hard-coded for now until MongoDB is setup:
+  if (page === "about") {
+    return [
+      {
+        name: "Contents",
+        sections: [
+          {
+            name: "All About Brandon",
+            fragmentID: "all-about-brandon"
+          },
+          {
+            name: "My Mindset",
+            fragmentID: "my-mindset"
+          }
+        ]
+      },
+      {
+        name: "References",
+        sections: [
+          {
+            name: "GitHub",
+            href: "https://github.com/branbarh"
+          }
+        ]
+      }
+    ];
+  }
   return [
     {
       name: "Contents",
